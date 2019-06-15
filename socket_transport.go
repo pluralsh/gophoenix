26fdec22bb9ccbb009b83ccb81cc765360ff867d
Edouard Roussillon <edouard.roussillon@gmail.com>
2019-06-14 21:26:35 -0300

    add log

--- socket_transport.go
+++ socket_transport.go
@@ -49,8 +49,6 @@ func (st *socketTransport) Connect(url url.URL, header http.Header, mr MessageRe
 	go st.writer()
 	go st.listen()
 
-	st.cr.NotifyConnect()
-
 	return err
 }
 
@@ -100,6 +98,7 @@ func (st *socketTransport) listen() {
 		st.stop()
 	}()
 	st.socket.SetReadLimit(maxMessageSize)
+	st.cr.NotifyConnect()
 	// st.socket.SetReadDeadline(time.Now().Add(pongWait))
 	// st.socket.SetPongHandler(func(string) error {
 	// 	st.socket.SetReadDeadline(time.Now().Add(pongWait))
